bf650eb58a9f84b628cf9ba1edd1166b1872f97d
mackibe <ndichujames2030@gmail.com>
2023-06-22 07:15:14 +0300

    updated but not finished with the configurations

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+//
 // import intializeApp function from firebase.app sub-package.
+//
 // Must intialize the the firebase app beofore using any firebase services getAuthFunction.
 import { intializeApp } from ('firebase/app');
+//
 // importing firease auth service.
 import { getAuth, onAuthStateChanges } from ('firebase/auth');
+//
 // importing the firestore service
 import { getFirestore, collection } from ('firebase/firestore');
+//
 // Creating firebase app
 const firebaseApp = intializeApp({
     apiKey: "AIzaSyB-w2Q2bX9m6sZLq_fiIfOU9dybVofM-G8",
@@ -15,15 +20,16 @@ const firebaseApp = intializeApp({
     appId: "1:167468057562:web:7b6455c8b8ba483ed836e3",
     measurementId: "G-VBRGZCTZ0N"    
 });
-
+//
 // create variables for getAuthfunctions
 const auth = getAuth(firebaseApp);
 const db = getAuth(firebaseApp);
 const todoCol = collection(db, 'todos');
 
-
+//
 //  Monitoring Auth state
 
+//
 // onAuthStateChanges function call back if a user is logged in
 onAuthStateChanges(auth, user => {
     if( user != null ) {
